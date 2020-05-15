9930a350af6f9c251b06df872704de54c95a31fa
Joao Neto <neto.com@msn.com>
2020-05-15 19:19:27 -0300

    Cabecalho

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,22 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System .in);
+		
+		//CABECALHO
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		System.out.println();
+		
+		
+	}
+	
+}
+
